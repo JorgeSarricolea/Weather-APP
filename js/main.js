8683c65c3667401c83aa62a78dc55855157d4666
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-11-01 02:41:17 -0600

    fix: api key and transfrom to js vanilla

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
-import { API_KEY } from './api-key.js';
-
-$(document).ready(function() {
-
-    let information = $("#information");
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=";
-    let emptyInput = "Input field cannot be empty";
-    let notFound = "Not city found:(";
+document.addEventListener("DOMContentLoaded", function () {
+    const information = document.getElementById("information");
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+    const emptyInput = "Input field cannot be empty";
+    const notFound = "Not city found:(";
+    const API_KEY = "33c9af543c6207cff3bfc0fdedc318b3";
 
     /* Search Button
     ---------------------*/
 
-    $(".search-btn").click(function() {
-        let userInp = $(".search-inp").val();
+    const searchBtn = document.querySelector(".search-btn");
+    searchBtn.addEventListener("click", fetchData);
+
+    /* Enter key press
+    ---------------------*/
+    const searchInp = document.querySelector(".search-inp");
+    searchInp.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            fetchData();
+        }
+    });
+
+    function fetchData() {
+        const userInp = searchInp.value;
 
         /* Error Message
         ---------------------*/
 
-        if (userInp.length == 0) {
-            $(".empty-input").html(emptyInput);
-            $(".empty-input").show();
-            setTimeout(function() {
-                $(".empty-input").hide();
+        if (userInp.length === 0) {
+            const emptyInputMessage = document.createElement("div");
+            emptyInputMessage.textContent = emptyInput;
+            emptyInputMessage.classList.add("empty-input");
+            information.appendChild(emptyInputMessage);
+            emptyInputMessage.style.display = "block";
+            setTimeout(function () {
+                emptyInputMessage.style.display = "none";
+                information.removeChild(emptyInputMessage);
             }, 3000);
 
         } else {
@@ -28,100 +42,76 @@ $(document).ready(function() {
             /* API Fetch GET
             ---------------------*/
 
-            $.ajax({
-                url: url + userInp + "&units=metric&appid=" + API_KEY,
-                success: function(data) {
-                    if (data != null) {
-                        $("#result").show();
-                        let details = data;
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url + userInp + "&units=metric&appid=" + API_KEY, true);
 
-                        let name = details.name;
-                        let weather = details.weather[0].main;
-                        let description = details.weather[0].description;
-                        let temperatureC = (details.main.temp).toFixed(0);
-                        let humidity = details.main.humidity;
-                        let windSpeed = (details.wind.speed*3.6).toFixed(2);
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    document.getElementById("result").style.display = "block";
+                    const details = JSON.parse(xhr.responseText);
 
-                        /*
-                        console.log(details);
-                        console.log(details.name);
-                        console.log(details.weather[0].main);
-                        console.log(details.weather[0].description);
-                        console.log(temperatureC);
-                        console.log(details.main.humidity);
-                        console.log(windSpeed);
-                        */
+                    const name = details.name;
+                    const weather = details.weather[0].main;
+                    const description = details.weather[0].description;
+                    const temperatureC = Math.round(details.main.temp);
+                    const humidity = details.main.humidity;
+                    const windSpeed = (details.wind.speed * 3.6).toFixed(2);
 
-                        console.log(details);
-
-                        /* Switch Weather Icons
-                        ---------------------*/
-
-                        switch (details.weather[0].main) {
-                            case "Thunderstorm":
-                                $(".weather-icon").attr("src", "https://openweathermap.org/img/wn/11d@2x.png");
-                                break;
-
-                            case "Drizzle":
-                            case "Rain":
-                                $(".weather-icon").attr("src", "https://openweathermap.org/img/wn/09d@2x.png");
-                                break;
-
-                            case "Snow":
-                                $(".weather-icon").attr("src", "https://openweathermap.org/img/wn/13d@2x.png");
-                                break;
-
-                            case "Mist":
-                                $(".weather-icon").attr("src", "https://openweathermap.org/img/wn/50d@2x.png");
-                                break;
-
-                            case "Clear":
-                                $(".weather-icon").attr("src", "https://openweathermap.org/img/wn/01d@2x.png");
-                                break;
-
-                            case "Clouds":
-                                $(".weather-icon").attr("src", "https://openweathermap.org/img/wn/02d@2x.png");
-                                break;
-
-                            default:
-                                $(".weather-icon").attr("src", "");
-                        }
-
-                        /* User City Search
-                        ---------------------*/
-
-                        information.html(
-                            `<div class="result">
-                                <h2>${name}</h2>
-                                <h3 class="temperature">${temperatureC}°C</h3>
-                                <h3>${weather} - ${description}</h3>
-                                <div class="weather-details">
-                                    <div class="container">
-                                        <i class="fa-solid fa-droplet">
-                                        </i><h3>Humidity: ${humidity}%</h3>
-                                    </div>
-                                    <div class="container">
-                                        <i class="fa-solid fa-wind"></i>
-                                        <h3>Wind speed: ${windSpeed} km/h</h3>
-                                    </div>
-                                </div>
-                            </div>`
-                        );
+                    switch (details.weather[0].main) {
+                        case "Thunderstorm":
+                            document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/11d@2x.png";
+                            break;
+                        case "Drizzle":
+                        case "Rain":
+                            document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/09d@2x.png";
+                            break;
+                        case "Snow":
+                            document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/13d@2x.png";
+                            break;
+                        case "Mist":
+                            document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/50d@2x.png";
+                            break;
+                        case "Clear":
+                            document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/01d@2x.png";
+                            break;
+                        case "Clouds":
+                            document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/02d@2x.png";
+                            break;
+                        default:
+                            document.querySelector(".weather-icon").src = "";
                     }
-                },
-                error: function() {
 
-                    /* API Invalid Input
-                    ---------------------*/
-
-                    $("#result").hide();
-                    $(".invalid-input").html(notFound);
-                    $(".invalid-input").show();
-                    setTimeout(function() {
-                            $(".invalid-input").hide();
-                    }, 3000);
+                    information.innerHTML = `
+                        <div class="result">
+                            <h2>${name}</h2>
+                            <h3 class="temperature">${temperatureC}°C</h3>
+                            <h3>${weather} - ${description}</h3>
+                            <div class="weather-details">
+                                <div class="container">
+                                    <i class="fa-solid fa-droplet"></i><h3>Humidity: ${humidity}%</h3>
+                                </div>
+                                <div class="container">
+                                    <i class="fa-solid fa-wind"></i><h3>Wind speed: ${windSpeed} km/h</h3>
+                                </div>
+                            </div>
+                        </div>`;
                 }
-            });
+            };
+
+            xhr.onerror = function () {
+                document.getElementById("result").style.display = "none";
+                const invalidInputMessage = document.createElement("div");
+                invalidInputMessage.textContent = notFound;
+                invalidInputMessage.classList.add("invalid-input");
+                information.appendChild(invalidInputMessage);
+                invalidInputMessage.style.display = "block";
+                setTimeout(function () {
+                    invalidInputMessage.style.display = "none";
+                    information.removeChild(invalidInputMessage);
+                }, 3000);
+            };
+
+            xhr.send();
         }
-    });
+    }
 });
