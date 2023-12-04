6ff279942a94f9183485ecc00a7c1b201ee6f0f9
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-12-04 00:28:21 -0600

    chore: modify README

--- README.md
+++ README.md
@@ -1,42 +1,64 @@
-# [Weather APP](https://jorgesarricolea.com/weather-app)
-This is a weather app, with a simple and responsive design, using the [OpenWeatherMap](https://openweathermap.org/api).
+# [Weather APP](https://wtrapp.netlify.app)
 
-## Description
-When working with REST APIs, the ideal project to practice is to make a weather app. I have used the free OpenWeatherMap API. The design is minimalist, and I have used the icons that this API contains. The app is programmed to show an error message and ask the user to try again if they enter an invalid value.
+¿Necesitas ver el clima de tu ciudad? pruéba con mi app del clima.
+
+## Table of Contents
+
+- [Weather APP](#weather-app)
+  - [Table of Contents](#table-of-contents)
+  - [Introduction](#introduction)
+  - [Getting Started](#getting-started)
+    - [Installation](#installation)
+  - [Features](#features)
+  - [Contribution](#contribution)
+  - [Copyright](#copyright)
+
+## Introduction
+
+When working with REST APIs, the ideal project to practice is to make a weather app. I have used the free OpenWeatherMap API.
 
 ## Getting Started
-To get started with this project, [click here](https://jorgesarricolea.com/weather-app) or simply clone this repository to your local machine and open the recipies-net.html file in your preferred web browser.
 
-Note: Unlike the project with MealDB, where an API key was not necessary, for this project, it is necessary to generate an API key. I recommend that you register on [OpenWeatherMap](https://openweathermap.org/api) and generate your key, so you only need to change the variables in case you want to run it locally. The variable is called "API_KEY" and you only need to declare that variable and assign it your API Key.
+To get started with "Weather-APP", follow these simple steps:
+
+**Note:** Unlike the project with MealDB, where an API key was not necessary, for this project, it is necessary to generate an API key. I recommend that you register on [OpenWeatherMap](https://openweathermap.org/api) and generate your key, so you only need to change the variables in case you want to run it locally. The variable is called "API_KEY" and you only need to declare that variable and assign it your API Key.
 
 Example:
 
 **let API_KEY = "your_api_key"**
 
 ```
-$(document).ready(function() {
-
-    let API_KEY = "your_api_key"
-    let information = $("#information");
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=";
-    let emptyInput = "Input field cannot be empty";
-    let notFound = "Not city found:(";
-    
-   // Rest of the code...
-});
+xhr.open("GET", url + userInp + "&units=metric&appid=" + API_KEY, true);
 ```
 
+### Installation
+
+1. Clone the repository to your local machine:
+
+```
+git clone https://github.com/JorgeSarricolea/Weather-APP
+```
+
+2. Navigate to the project directory:
+
+```
+cd Weather-APP
+```
+
+3. Open the index.html file with Live Server (In VSCode) or with your favorite browser.
+
 ## Features
+
 Some of the key features of this project include:
 
-- Full responsive design.
-- Custom-made CSS styles and jQuery JavaScript.
+- **Responsividad:** The UI works for mobile devices and desktop.
+- **Fetching de datos**: Gets the data needed to display from the API.
+- **Keypress:** It works by pressing the search button or the "enter" key.
 
-## Contributing
+## Contribution
+
 While contributions to this project are welcome. If you would like to contribute, feel free to fork this repository and submit a pull request.
 
-## Credits
+## Copyright
+
 This project was created by [Jorge Sarricolea](https://jorgesarricolea.com). if you have any specific code questions or would like to chat about programming, feel free to contact me on [WhatsApp](https://wa.me/529381095593).
-
-
-
--- js/main.js
+++ js/main.js
@@ -1,70 +1,72 @@
+// API KEY
+const API_KEY = "33c9af543c6207cff3bfc0fdedc318b3";
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Get references to HTML elements
-    const information = document.getElementById("information");
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=";
-    const emptyInput = "Input field cannot be empty";
-    const notFound = "Not city found:(";
-    const API_KEY = "33c9af543c6207cff3bfc0fdedc318b3";
+  // Get references to HTML elements
+  const information = document.getElementById("information");
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const emptyInput = "Input field cannot be empty";
+  const notFound = "Not city found:(";
 
-    // Search Button
-    const searchBtn = document.querySelector(".search-btn");
-    searchBtn.addEventListener("click", fetchData);
+  // Search Button
+  const searchBtn = document.querySelector(".search-btn");
+  searchBtn.addEventListener("click", fetchData);
 
-    // Enter key press
-    const searchInp = document.querySelector(".search-inp");
-    searchInp.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") fetchData();
-    });
+  // Enter key press
+  const searchInp = document.querySelector(".search-inp");
+  searchInp.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") fetchData();
+  });
 
-    // Function to fetch weather data
-    function fetchData() {
-        const userInp = searchInp.value;
+  // Function to fetch weather data
+  function fetchData() {
+    const userInp = searchInp.value;
 
-        // Error Message
-        if (userInp.length === 0) {
-            const emptyInputMessage = document.createElement("div");
-            emptyInputMessage.textContent = emptyInput;
-            emptyInputMessage.classList.add("empty-input");
-            information.appendChild(emptyInputMessage);
-            emptyInputMessage.style.display = "block";
-            setTimeout(function () {
-                emptyInputMessage.style.display = "none";
-                information.removeChild(emptyInputMessage);
-            }, 3000);
-        } else {
-            const xhr = new XMLHttpRequest();
-            xhr.open("GET", url + userInp + "&units=metric&appid=" + API_KEY, true);
+    // Error Message
+    if (userInp.length === 0) {
+      const emptyInputMessage = document.createElement("div");
+      emptyInputMessage.textContent = emptyInput;
+      emptyInputMessage.classList.add("empty-input");
+      information.appendChild(emptyInputMessage);
+      emptyInputMessage.style.display = "block";
+      setTimeout(function () {
+        emptyInputMessage.style.display = "none";
+        information.removeChild(emptyInputMessage);
+      }, 3000);
+    } else {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url + userInp + "&units=metric&appid=" + API_KEY, true);
 
-            xhr.onload = function () {
-                if (xhr.status === 200) {
-                    document.getElementById("result").style.display = "block";
-                    const details = JSON.parse(xhr.responseText);
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          document.getElementById("result").style.display = "block";
+          const details = JSON.parse(xhr.responseText);
 
-                    const name = details.name;
-                    const weather = details.weather[0].main;
-                    const description = details.weather[0].description;
-                    const temperatureC = Math.round(details.main.temp);
-                    const humidity = details.main.humidity;
-                    const windSpeed = (details.wind.speed * 3.6).toFixed(2);
+          const name = details.name;
+          const weather = details.weather[0].main;
+          const description = details.weather[0].description;
+          const temperatureC = Math.round(details.main.temp);
+          const humidity = details.main.humidity;
+          const windSpeed = (details.wind.speed * 3.6).toFixed(2);
 
-                    // Define a hash table for weather icons
-                    const weatherIcons = {
-                        Thunderstorm: "https://openweathermap.org/img/wn/11d@2x.png",
-                        Drizzle: "https://openweathermap.org/img/wn/09d@2x.png",
-                        Rain: "https://openweathermap.org/img/wn/09d@2x.png",
-                        Snow: "https://openweathermap.org/img/wn/13d@2x.png",
-                        Mist: "https://openweathermap.org/img/wn/50d@2x.png",
-                        Clear: "https://openweathermap.org/img/wn/01d@2x.png",
-                        Clouds: "https://openweathermap.org/img/wn/02d@2x.png"
-                    };
+          // Define a hash table for weather icons
+          const weatherIcons = {
+            Thunderstorm: "https://openweathermap.org/img/wn/11d@2x.png",
+            Drizzle: "https://openweathermap.org/img/wn/09d@2x.png",
+            Rain: "https://openweathermap.org/img/wn/09d@2x.png",
+            Snow: "https://openweathermap.org/img/wn/13d@2x.png",
+            Mist: "https://openweathermap.org/img/wn/50d@2x.png",
+            Clear: "https://openweathermap.org/img/wn/01d@2x.png",
+            Clouds: "https://openweathermap.org/img/wn/02d@2x.png",
+          };
 
-                    // Get the appropriate weather icon URL from the hash table
-                    const weatherIconUrl = weatherIcons[weather] || "";
+          // Get the appropriate weather icon URL from the hash table
+          const weatherIconUrl = weatherIcons[weather] || "";
 
-                    document.querySelector(".weather-icon").src = weatherIconUrl;
+          document.querySelector(".weather-icon").src = weatherIconUrl;
 
-                    // Display weather information
-                    information.innerHTML = `<div class="result">
+          // Display weather information
+          information.innerHTML = `<div class="result">
                         <h2>${name}</h2>
                         <h3 class="temperature">${temperatureC}°C</h3>
                         <h3>${weather} - ${description}</h3>
@@ -77,23 +79,23 @@ document.addEventListener("DOMContentLoaded", function () {
                             </div>
                         </div>
                     </div>`;
-                }
-            };
-
-            xhr.onerror = function () {
-                document.getElementById("result").style.display = "none";
-                const invalidInputMessage = document.createElement("div");
-                invalidInputMessage.textContent = notFound;
-                invalidInputMessage.classList.add("invalid-input");
-                information.appendChild(invalidInputMessage);
-                invalidInputMessage.style.display = "block";
-                setTimeout(function () {
-                    invalidInputMessage.style.display = "none";
-                    information.removeChild(invalidInputMessage);
-                }, 3000);
-            };
-
-            xhr.send();
         }
+      };
+
+      xhr.onerror = function () {
+        document.getElementById("result").style.display = "none";
+        const invalidInputMessage = document.createElement("div");
+        invalidInputMessage.textContent = notFound;
+        invalidInputMessage.classList.add("invalid-input");
+        information.appendChild(invalidInputMessage);
+        invalidInputMessage.style.display = "block";
+        setTimeout(function () {
+          invalidInputMessage.style.display = "none";
+          information.removeChild(invalidInputMessage);
+        }, 3000);
+      };
+
+      xhr.send();
     }
+  }
 });
